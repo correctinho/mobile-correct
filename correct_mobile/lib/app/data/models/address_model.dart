c6f9f2e0a9e48b7d30674c171c8b52068f46dc1b
Gabriel Lima <gabriellglma9@gmail.com>
2024-08-20 14:20:25 -0300

    new models the infor adicional

--- correct_mobile/lib/app/data/models/address_model.dart
+++ correct_mobile/lib/app/data/models/address_model.dart
@@ -1,15 +1,9 @@
+import 'dart:convert';
+
 import 'package:mobile_create/app/domain/entities/adress_entity.dart';
 
 class AddressModel extends AddressEntity {
-  AddressModel(
-      {required super.zipCode,
-      required super.street,
-      required super.neighborhood,
-      required super.city,
-      required super.state,
-      super.number,
-      super.complement,
-      super.country});
+  AddressModel({required super.zipCode, required super.street, required super.neighborhood, required super.city, required super.state, super.number, super.complement, super.country});
 
   factory AddressModel.fromViaCep(Map<String, dynamic> map) {
     return AddressModel(
@@ -20,4 +14,19 @@ class AddressModel extends AddressEntity {
       neighborhood: map['bairro'],
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'zipCode': zipCode,
+      'street': street,
+      'neighborhood': neighborhood,
+      'city': city,
+      'state': state,
+      'number': number,
+      'complement': complement,
+      'country': country,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
 }
---
+++ correct_mobile/lib/app/data/models/user_register_model.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserIdentityInfoModel {
+  String document;
+  String email;
+  String password;
+  UserIdentityInfoModel({
+    required this.document,
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'document': document,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+}
+
+class UserAdditionalInfoModel {
+  String fullname;
+  String phone;
+  String dateOfBirth;
+  String gender;
+  int salary;
+  String function;
+  String recommendationCode;
+  String maritalStatus;
+  int dependentsQuantity;
+  String document2;
+  String document3;
+  UserAdditionalInfoModel({
+    required this.fullname,
+    required this.phone,
+    required this.dateOfBirth,
+    required this.gender,
+    required this.salary,
+    required this.function,
+    required this.recommendationCode,
+    required this.maritalStatus,
+    required this.dependentsQuantity,
+    required this.document2,
+    required this.document3,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'full_name': fullname,
+      'phone': phone,
+      'date_of_birth': dateOfBirth,
+      'gender': gender,
+      'salary': salary,
+      'function': function,
+      'recommendationCode': recommendationCode,
+      'maritalStatus': maritalStatus,
+      'dependents_quantity': dependentsQuantity,
+      'document2': document2,
+      'documente3': document3,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+}
+
+class AdditionalDocumentsModel {
+  String selfieBase64;
+  String documentFrontBase64;
+  String documentBackBase64;
+  String documentSelfieBase64;
+  AdditionalDocumentsModel({
+    required this.selfieBase64,
+    required this.documentFrontBase64,
+    required this.documentBackBase64,
+    required this.documentSelfieBase64,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'selfieBase64': selfieBase64,
+      'documentFrontBase64': documentFrontBase64,
+      'documentBackBase64': documentBackBase64,
+      'documentSelfieBase64': documentSelfieBase64,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+}
